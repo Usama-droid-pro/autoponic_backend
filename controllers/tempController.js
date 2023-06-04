0b4d4dea4c795d500f980bcf04ef78fb9c965ff3
Usama <uk5458622@gmail.com>
2023-06-04 13:52:24 +0500

    commit latest

--- controllers/tempController.js
+++ controllers/tempController.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const tempModel = require("../models/tempratureModel");
 const humidityModel = require('../models/humidityModel')
+const moistureModel = require('../models/moistureModel')
+
 var format = require("date-fns/format");
 
 exports.getTempReadings = async (req, res) => {
@@ -30,25 +32,42 @@ exports.getTempReadings = async (req, res) => {
 exports.getMoistureValue = async (req, res) => {
 	const today = new Date();
 	today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
-	console.log("tempValues");
 	let tomorrow = new Date(today);
 	
 	tomorrow.setDate(today.getDate() + 1);
-	const temp = await tempModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
-	const count = await tempModel.countDocuments({ createdAt: { $gte: today, $lt: tomorrow } });
-	const temp2 = JSON.parse(JSON.stringify(temp));
+	const moisture = await moistureModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
+	const moisture2 = JSON.parse(JSON.stringify(moisture));
 
-
-
-
-	temp2.map((ele) => {
+	moisture2.map((ele) => {
 		ele.time2 = format(new Date(ele.createdAt), "hh:mm a");
 	});
 	
-	if (!temp) {
+	if (!moisture2) {
 		throw new Error(204, "No Content");
 	}
-	res.status(200).json({ temp2, count });
+	res.status(200).json({ moisture2 });
+};
+
+exports.getMoistureGraph = async (req, res) => {
+	const today = new Date();
+	today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
+	let tomorrow = new Date(today);
+	
+	tomorrow.setDate(today.getDate() + 1);
+	const moisture = await moistureModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
+	const moisture2 = JSON.parse(JSON.stringify(moisture));
+
+	moisture2.map((ele) => {
+		ele.time2 = format(new Date(ele.createdAt), "hh:mm a");
+	});
+
+	const result = await fetchThreeHourIntervals(moisture2);
+	console.log(result)
+	
+	if (!result) {
+		throw new Error(204, "No Content");
+	}
+	res.status(200).json({ result });
 };
 
 
@@ -168,8 +187,6 @@ exports.humidityGraph = async (req, res) => {
 	const humid2 = JSON.parse(JSON.stringify(humid));
 
 
-
-
 	humid2.map((ele) => {
 		ele.time2 = format(new Date(ele.createdAt), "hh:mm a");
 	});
@@ -182,3 +199,55 @@ exports.humidityGraph = async (req, res) => {
 	}
 	res.status(200).json({ result });
 };
+
+
+async function fetchThreeHourIntervals(records) {
+	const intervals = {};
+	let intervalEnd;
+	// Iterate over each record
+	records.forEach(record => {
+	  // Extract the hour and period (AM/PM) from the time2 field
+	  const [time, period] = record.time2.split(' ');
+	  const [hour, minutes] = time.split(':');
+	  
+	  // Convert the hour to 24-hour format
+	  let hour24 = parseInt(hour, 10);
+	  if (period === 'PM' && hour24 !== 12) {
+		hour24 += 12;
+	  } else if (period === 'AM' && hour24 === 12) {
+		hour24 = 0;
+	  }
+	  
+	  // Calculate the interval for the record
+	  const intervalStart = Math.floor(hour24 / 3) * 3;
+	   intervalEnd = (intervalStart + 3) % 24;
+	  
+	  // Create the interval key if it doesn't exist
+	  if (!intervals[intervalStart]) {
+		intervals[intervalStart] = {
+		  count: 0,
+		  sum: 0,
+		};
+	  }
+	  
+	  // Add the temperature to the interval
+	  intervals[intervalStart].sum += record.value;
+	  intervals[intervalStart].count++;
+	});
+	
+	// Calculate the average for each interval
+	const averages = {};
+	for (let i = 0; i < 24; i += 3) {
+	  const intervalStart = i;
+	  const intervalEnd = (i + 3) % 24;
+	  const intervalKey = `${intervalStart}:00 ${intervalStart < 12 ? 'AM' : 'PM'} - ${intervalEnd}:00 ${intervalEnd < 12 ? 'AM' : 'PM'}`;
+	  if (intervals[intervalStart]) {
+		averages[intervalKey] = intervals[intervalStart].sum / intervals[intervalStart].count;
+	  } else {
+		averages[intervalKey] = 0;
+	  }
+	}
+	
+	return averages;
+  }
+  

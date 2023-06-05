9e190205a994653d9790af85299f6ad0a28d0c57
Hasnat313 <hasnatsaghir@gmail.com>
2023-06-05 14:20:45 +0500

    Data fns timezone changes

--- controllers/tempController.js
+++ controllers/tempController.js
@@ -1,28 +1,30 @@
 const mongoose = require("mongoose");
 const tempModel = require("../models/tempratureModel");
-const humidityModel = require('../models/humidityModel')
-const moistureModel = require('../models/moistureModel')
+const humidityModel = require("../models/humidityModel");
+const moistureModel = require("../models/moistureModel");
 
-var format = require("date-fns/format");
+const { format, parseISO } = require("date-fns");
+const { zonedTimeToUtc, utcToZonedTime } = require("date-fns-tz");
 
 exports.getTempReadings = async (req, res) => {
 	const today = new Date();
 	today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
 	console.log("tempValues");
 	let tomorrow = new Date(today);
-	
+
 	tomorrow.setDate(today.getDate() + 1);
 	const temp = await tempModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
 	const count = await tempModel.countDocuments({ createdAt: { $gte: today, $lt: tomorrow } });
 	const temp2 = JSON.parse(JSON.stringify(temp));
 
-
-
-
 	temp2.map((ele) => {
-		ele.time2 = format(new Date(ele.createdAt), "hh:mm a");
+		const createdAtUtc = utcToZonedTime(ele.createdAt, "Asia/Karachi");
+		const createdAtLocal = zonedTimeToUtc(createdAtUtc, "Asia/Karachi");
+
+		ele.temp2 = format(createdAtLocal, "yyyy-MM-dd HH:mm:ss a");
+		console.log(ele.temp2);
 	});
-	
+
 	if (!temp) {
 		throw new Error(204, "No Content");
 	}
@@ -33,7 +35,7 @@ exports.getMoistureValue = async (req, res) => {
 	const today = new Date();
 	today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
 	let tomorrow = new Date(today);
-	
+
 	tomorrow.setDate(today.getDate() + 1);
 	const moisture = await moistureModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
 	const moisture2 = JSON.parse(JSON.stringify(moisture));
@@ -41,7 +43,7 @@ exports.getMoistureValue = async (req, res) => {
 	moisture2.map((ele) => {
 		ele.time2 = format(new Date(ele.createdAt), "hh:mm a");
 	});
-	
+
 	if (!moisture2) {
 		throw new Error(204, "No Content");
 	}
@@ -52,7 +54,7 @@ exports.getMoistureGraph = async (req, res) => {
 	const today = new Date();
 	today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
 	let tomorrow = new Date(today);
-	
+
 	tomorrow.setDate(today.getDate() + 1);
 	const moisture = await moistureModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
 	const moisture2 = JSON.parse(JSON.stringify(moisture));
@@ -62,14 +64,13 @@ exports.getMoistureGraph = async (req, res) => {
 	});
 
 	const result = await fetchThreeHourIntervals(moisture2);
-	console.log(result)
-	
+	console.log(result);
+
 	if (!result) {
 		throw new Error(204, "No Content");
 	}
 	res.status(200).json({ result });
 };
-
 
 exports.getTempGraph = async (req, res) => {
 	const today = new Date();
@@ -77,12 +78,11 @@ exports.getTempGraph = async (req, res) => {
 	today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
 	console.log("tempValues");
 	let tomorrow = new Date(today);
-	
+
 	tomorrow.setDate(today.getDate() + 1);
 	const temp = await tempModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
 	const count = await tempModel.countDocuments({ createdAt: { $gte: today, $lt: tomorrow } });
 	const temp2 = JSON.parse(JSON.stringify(temp));
-
 
 	temp2.map((ele) => {
 		ele.time2 = format(new Date(ele.createdAt), "hh:mm a");
@@ -90,84 +90,82 @@ exports.getTempGraph = async (req, res) => {
 
 	const result = await fetchThreeHourIntervals(temp2);
 
-	console.log(result)
-	
+	console.log(result);
+
 	if (!result) {
 		throw new Error(204, "No Content");
 	}
 	res.status(200).json({ result, count });
 };
 
-
 async function fetchThreeHourIntervals(records) {
 	const intervals = {};
 	let intervalEnd;
 	// Iterate over each record
-	records.forEach(record => {
-	  // Extract the hour and period (AM/PM) from the time2 field
-	  const [time, period] = record.time2.split(' ');
-	  const [hour, minutes] = time.split(':');
-	  
-	  // Convert the hour to 24-hour format
-	  let hour24 = parseInt(hour, 10);
-	  if (period === 'PM' && hour24 !== 12) {
-		hour24 += 12;
-	  } else if (period === 'AM' && hour24 === 12) {
-		hour24 = 0;
-	  }
-	  
-	  // Calculate the interval for the record
-	  const intervalStart = Math.floor(hour24 / 3) * 3;
-	   intervalEnd = (intervalStart + 3) % 24;
-	  
-	  // Create the interval key if it doesn't exist
-	  if (!intervals[intervalStart]) {
-		intervals[intervalStart] = {
-		  count: 0,
-		  sum: 0,
-		};
-	  }
-	  
-	  // Add the temperature to the interval
-	  intervals[intervalStart].sum += record.value;
-	  intervals[intervalStart].count++;
+	records.forEach((record) => {
+		// Extract the hour and period (AM/PM) from the time2 field
+		const [time, period] = record.time2.split(" ");
+		const [hour, minutes] = time.split(":");
+
+		// Convert the hour to 24-hour format
+		let hour24 = parseInt(hour, 10);
+		if (period === "PM" && hour24 !== 12) {
+			hour24 += 12;
+		} else if (period === "AM" && hour24 === 12) {
+			hour24 = 0;
+		}
+
+		// Calculate the interval for the record
+		const intervalStart = Math.floor(hour24 / 3) * 3;
+		intervalEnd = (intervalStart + 3) % 24;
+
+		// Create the interval key if it doesn't exist
+		if (!intervals[intervalStart]) {
+			intervals[intervalStart] = {
+				count: 0,
+				sum: 0,
+			};
+		}
+
+		// Add the temperature to the interval
+		intervals[intervalStart].sum += record.value;
+		intervals[intervalStart].count++;
 	});
-	
+
 	// Calculate the average for each interval
 	const averages = {};
 	for (let i = 0; i < 24; i += 3) {
-	  const intervalStart = i;
-	  const intervalEnd = (i + 3) % 24;
-	  const intervalKey = `${intervalStart}:00 ${intervalStart < 12 ? 'AM' : 'PM'} - ${intervalEnd}:00 ${intervalEnd < 12 ? 'AM' : 'PM'}`;
-	  if (intervals[intervalStart]) {
-		averages[intervalKey] = intervals[intervalStart].sum / intervals[intervalStart].count;
-	  } else {
-		averages[intervalKey] = 0;
-	  }
+		const intervalStart = i;
+		const intervalEnd = (i + 3) % 24;
+		const intervalKey = `${intervalStart}:00 ${
+			intervalStart < 12 ? "AM" : "PM"
+		} - ${intervalEnd}:00 ${intervalEnd < 12 ? "AM" : "PM"}`;
+		if (intervals[intervalStart]) {
+			averages[intervalKey] = intervals[intervalStart].sum / intervals[intervalStart].count;
+		} else {
+			averages[intervalKey] = 0;
+		}
 	}
-	
+
 	return averages;
-  }
-  
-  exports.getHumidityReadings = async (req, res) => {
+}
+
+exports.getHumidityReadings = async (req, res) => {
 	const today = new Date();
 
 	today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
 	console.log("tempValues");
 	let tomorrow = new Date(today);
-	
+
 	tomorrow.setDate(today.getDate() + 1);
 	const humid = await humidityModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
 	// const humid= await humidityModel.countDocuments({ createdAt: { $gte: today, $lt: tomorrow } });
 	const humid2 = JSON.parse(JSON.stringify(humid));
 
-
-
-
 	humid2.map((ele) => {
 		ele.time2 = format(new Date(ele.createdAt), "hh:mm a");
 	});
-	
+
 	if (!humid) {
 		throw new Error(204, "No Content");
 	}
@@ -180,74 +178,73 @@ exports.humidityGraph = async (req, res) => {
 	today.setHours(0, 0, 0, 0); // Set time to the beginning of the day
 	console.log("tempValues");
 	let tomorrow = new Date(today);
-	
+
 	tomorrow.setDate(today.getDate() + 1);
 	const humid = await humidityModel.find({ createdAt: { $gte: today, $lt: tomorrow } });
 	// const humid= await humidityModel.countDocuments({ createdAt: { $gte: today, $lt: tomorrow } });
 	const humid2 = JSON.parse(JSON.stringify(humid));
-
 
 	humid2.map((ele) => {
 		ele.time2 = format(new Date(ele.createdAt), "hh:mm a");
 	});
 
 	const result = await fetchThreeHourIntervals(humid2);
-	console.log(result)
-	
+	console.log(result);
+
 	if (!result) {
 		throw new Error(204, "No Content");
 	}
 	res.status(200).json({ result });
 };
 
-
 async function fetchThreeHourIntervals(records) {
 	const intervals = {};
 	let intervalEnd;
 	// Iterate over each record
-	records.forEach(record => {
-	  // Extract the hour and period (AM/PM) from the time2 field
-	  const [time, period] = record.time2.split(' ');
-	  const [hour, minutes] = time.split(':');
-	  
-	  // Convert the hour to 24-hour format
-	  let hour24 = parseInt(hour, 10);
-	  if (period === 'PM' && hour24 !== 12) {
-		hour24 += 12;
-	  } else if (period === 'AM' && hour24 === 12) {
-		hour24 = 0;
-	  }
-	  
-	  // Calculate the interval for the record
-	  const intervalStart = Math.floor(hour24 / 3) * 3;
-	   intervalEnd = (intervalStart + 3) % 24;
-	  
-	  // Create the interval key if it doesn't exist
-	  if (!intervals[intervalStart]) {
-		intervals[intervalStart] = {
-		  count: 0,
-		  sum: 0,
-		};
-	  }
-	  
-	  // Add the temperature to the interval
-	  intervals[intervalStart].sum += record.value;
-	  intervals[intervalStart].count++;
+	records.forEach((record) => {
+		// Extract the hour and period (AM/PM) from the time2 field
+		const [time, period] = record.time2.split(" ");
+		const [hour, minutes] = time.split(":");
+
+		// Convert the hour to 24-hour format
+		let hour24 = parseInt(hour, 10);
+		if (period === "PM" && hour24 !== 12) {
+			hour24 += 12;
+		} else if (period === "AM" && hour24 === 12) {
+			hour24 = 0;
+		}
+
+		// Calculate the interval for the record
+		const intervalStart = Math.floor(hour24 / 3) * 3;
+		intervalEnd = (intervalStart + 3) % 24;
+
+		// Create the interval key if it doesn't exist
+		if (!intervals[intervalStart]) {
+			intervals[intervalStart] = {
+				count: 0,
+				sum: 0,
+			};
+		}
+
+		// Add the temperature to the interval
+		intervals[intervalStart].sum += record.value;
+		intervals[intervalStart].count++;
 	});
-	
+
 	// Calculate the average for each interval
 	const averages = {};
 	for (let i = 0; i < 24; i += 3) {
-	  const intervalStart = i;
-	  const intervalEnd = (i + 3) % 24;
-	  const intervalKey = `${intervalStart}:00 ${intervalStart < 12 ? 'AM' : 'PM'} - ${intervalEnd}:00 ${intervalEnd < 12 ? 'AM' : 'PM'}`;
-	  if (intervals[intervalStart]) {
-		averages[intervalKey] = intervals[intervalStart].sum / intervals[intervalStart].count;
-	  } else {
-		averages[intervalKey] = 0;
-	  }
+		const intervalStart = i;
+		const intervalEnd = (i + 3) % 24;
+		const intervalKey = `${intervalStart}:00 ${
+			intervalStart < 12 ? "AM" : "PM"
+		} - ${intervalEnd}:00 ${intervalEnd < 12 ? "AM" : "PM"}`;
+		if (intervals[intervalStart]) {
+			averages[intervalKey] = intervals[intervalStart].sum / intervals[intervalStart].count;
+		} else {
+			averages[intervalKey] = 0;
+		}
 	}
-	
+
 	return averages;
-  }
-  
+}
